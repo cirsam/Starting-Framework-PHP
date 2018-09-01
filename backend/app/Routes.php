72dee3c1e3427b96971052b2125aaf42cd7b8129
Samuel Antwi <samuelantwi@Samuels-MacBook-Pro.local>
2018-09-01 08:28:04 -0400

    Moved things around

--- backend/app/Routes.php
+++ backend/app/Routes.php
@@ -16,23 +16,19 @@ Class Routes
 	{
 		if(count($request)==0)
 		{
-			echo "got nere";
 			$useMethods[0] = $this->controller;
 			$useMethods[1] = $this->method;			
 		}
 		else
 		{
-			echo "got nere2";
-
 			$useMethods = explode("/", rtrim($request['uri']));
-			//$useMethods = $useMethod;
 
 			if($useMethods[0] == '')
 			{
 				$useMethods[0] = $this->controller;
 			}
-			
-			if(!isset($useMethods[1]))
+
+			if(!isset($useMethods[1]) || $useMethods[1]=="")
 			{
 				$useMethods[1] = $this->method;			
 			}
@@ -46,7 +42,7 @@ Class Routes
 		
 		try
 		{
-			$ret_value = call_user_func_array(array($class, $useMethods[1]), $params); // As of PHP 5.3.0						
+			call_user_func_array(array($class, $useMethods[1]), $params); // As of PHP 5.3.0						
 		}
 		catch(Exception $e)
 		{
--- backend/index.php
+++ backend/index.php
@@ -1,5 +1,5 @@
 <?php
-    require_once __DIR__ . '/backend/vendor/autoload.php';
+    require_once __DIR__ . '/vendor/autoload.php';
 
     use Bidding\Routes as Routes;
     $route = new Routes();
